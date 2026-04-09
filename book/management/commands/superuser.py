ff7713d9b46a6704def42cfc07105a6658af8e43
akm-vvvr <akmpg001@gmail.com>
2026-04-09 15:19:13 +0900

    3rd

--- book/management/commands/superuser.py
+++ book/management/commands/superuser.py
@@ -8,7 +8,7 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         if not User.objects.filter(username='akm-sss').exists():
             User.objects.create_superuser(
-                username='akm-sss',
+                username='akm-vvvr',
                 email='',
                 password='gear-sys25'
             )
